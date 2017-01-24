7eeb9bdf6625e9e33bd53766f52e46d18d4a4614
Anatolii Chmykhalo <tolya.double@gmail.com>
2017-01-24 22:07:46 +0300

    Added "foreman" support

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 # puppetexplore
-
 Script to sync Puppet nodes information to Device42 (http://device42.com)
-
-This script was tested with Puppet 3.8.1 and Puppet 4.2.
-
+This script was tested with Puppet 3.8.1, Puppet 4.2. and Foreman 1.14.0.
+You may use scripts for direct Puppet or/and Foreman discoveries.
 
 # Requirements
-
-
-# Configure
-
 Take the file `settings.yaml.example` and rename it to `settings.yaml`. Then change the settings to correct ones.
 
+# Puppet Configure
 You should get from the Puppet server (register client):
 
 * client's name
@@ -30,13 +25,18 @@ See files "auth.conf" and "/etc/puppetlabs/puppetserver/conf.d/ca.conf" (Puppet4
 
 See [NodeFilter.md](./NodeFilter.md) for node filtering options.
 
+# Foreman Configure
+Client there should be allowed connect to foreman API endpoints.
 
 # Run
-
+Puppet :
 ```
-    python puppetexplore.py [-c /path/to/settings.yaml]
+python puppetexplore.py [-c /path/to/settings.yaml]
 ```
-
+Foreman :
+```
+python foremanexplore.py [-c /path/to/settings.yaml]
+```
 # Bugs / Feature Requests
 
 Please attach node info from puppet/facter while sending bugs/feature requests. It can help to understand your specifics.
---
+++ foremanexplore.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+
+DESCRIPTION = """
+    Script file puppetexplore
+
+    Author: Anatolii Chmykhalo, 2017
+
+"""
+
+import os
+import sys
+import logging
+import json
+import re
+import ast
+import argparse
+import device42
+import requests
+
+from foreman.client import Foreman
+from puppetexplore import JSONEncoder, get_config, d42_update
+requests.packages.urllib3.disable_warnings()
+logging.getLogger('foreman.client').setLevel(logging.CRITICAL)
+
+logger = logging.getLogger('log')
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+
+parser = argparse.ArgumentParser(description="puppetexplore", epilog=DESCRIPTION)
+
+parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output')
+parser.add_argument('-q', '--quiet', action='store_true', help='Quiet mode - outputs only errors')
+parser.add_argument('-c', '--config', help='Config file', default='settings.yaml')
+parser.add_argument('-S', '--savenodes', help='Save nodes info from Puppet server to json file')
+parser.add_argument('-n', '--onlynode', action='append', help='Process only selected nodes (fqdn or hostname)')
+
+debugmode = False
+cpuf_re = re.compile(r'@ ([\w\d\.]+)GHz', re.I)
+
+
+def main():
+    global debugmode
+    args = parser.parse_args()
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+        debugmode = True
+    if args.quiet:
+        logger.setLevel(logging.ERROR)
+        debugmode = False
+    onlynodes = []
+    if args.onlynode:
+        onlynodes = args.onlynode
+
+    config = get_config(args.config)
+    host = config['foreman']['host']
+    user = config['foreman']['user']
+    password = config['foreman']['pass']
+
+    f = Foreman('https://%s' % host, (user, password), verify=False)
+    node_ids = []
+    for node in f.hosts.index():
+        node = node['host']
+        if len(onlynodes) > 0 and node['name'] in onlynodes:
+            node_ids.append(node['id'])
+        elif len(onlynodes) == 0:
+            node_ids.append(node['id'])
+
+    facts_query = 'fqdn or memorysize_mb or is_virtual or processorcount or processors::models'
+
+    nodes = []
+    for node_id in node_ids:
+
+        host = f.hosts.show(id=node_id)['host']
+
+        if len(onlynodes) > 0 and host['name'] not in onlynodes:
+            continue
+
+        host['model'] = f.models.show(id=host['model_id'])
+        host['os'] = f.operatingsystems.show(id=host['operatingsystem_id'])
+        facts = f.do_get('/api/hosts/%s/facts?search=%s&per_page=999' % (node_id, facts_query), {})
+        facts = facts[host['name']] if host['name'] in facts else {}
+        disks = f.do_get('/api/hosts/%s/facts?search=disks&per_page=999' % node_id, {})
+        disks = disks[host['name']] if host['name'] in disks else {}
+        ec2_metadata = f.do_get('/api/hosts/%s/facts?search=ec2_metadata&per_page=999' % node_id, {})
+        ec2_metadata = ec2_metadata[host['name']] if host['name'] in ec2_metadata else {}
+        networking = f.do_get('/api/hosts/%s/facts?search=networking&per_page=999' % node_id, {})
+        networking = networking[host['name']] if host['name'] in networking else {}
+
+        formatted_disks = {}
+        for key in disks:
+            splitted = key.split('::')
+            if len(splitted) == 3:
+                if splitted[2] == 'size_bytes':
+                    formatted_disks[splitted[1]] = {
+                        'size_bytes': disks[key]
+                    }
+
+        formatted_interfaces = {}
+        for key in networking:
+            splitted = key.split('::')
+            if len(splitted) == 4:
+                if splitted[2] not in formatted_interfaces:
+                    formatted_interfaces[splitted[2]] = {}
+                formatted_interfaces[splitted[2]].update({splitted[3]: networking[key]})
+
+        # prepare correct format
+        data = {
+            'hostname': host['name'],
+            'memorysize_mb': facts['memorysize_mb'],
+            'fqdn': facts['fqdn'],
+            'disks': formatted_disks,
+            'is_virtual': facts['is_virtual'],
+            'physicalprocessorcount': facts['physicalprocessorcount'],
+            'processorcount': facts['processorcount'],
+            'processors': {
+                'models': ast.literal_eval(facts['processors::models'])
+            },
+            'operatingsystem': host['os']['operatingsystem']['name'],
+            'operatingsystemrelease': host['os']['operatingsystem']['release_name'],
+            'macaddress': host['mac'],
+            'networking': {
+                'interfaces': formatted_interfaces
+            }
+        }
+        if len(ec2_metadata) > 0:
+            data.update({'ec2_metadata': ec2_metadata})
+
+        nodes.append(data)
+
+    if args.savenodes:
+        with open(args.savenodes, 'w') as wnf:
+            wnf.write(json.dumps(nodes, cls=JSONEncoder, indent=4, sort_keys=True, ensure_ascii=False))
+
+    dev42 = device42.Device42(
+        endpoint=config['device42']['host'],
+        user=config['device42']['user'],
+        password=config['device42']['pass'],
+        logger=logger,
+        debug=debugmode
+    )
+
+    d42_update(dev42, nodes, config['options'], config.get('static', {}),
+               from_version='4', puppethost=config['puppet_server']['host'])
+
+    return 0
+
+if __name__ == "__main__":
+    retval = main()
+    print 'Done'
+    sys.exit(retval)
